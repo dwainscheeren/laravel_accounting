4797f6976c3018d064c3aa1f4ca2bb4866d2ed37
Daan van Berkel <info@daanvanberkel.nl>
2020-01-11 10:32:05 +0100

    All the things are typed now

--- src/AccountingProvider.php
+++ src/AccountingProvider.php
@@ -2,10 +2,38 @@
 namespace Dvb\Accounting;
 
 interface AccountingProvider {
+    /**
+     * @return AccountingRelation[]
+     */
     public function getRelations(): array;
+
+    /**
+     * @return AccountingLedger[]
+     */
     public function getLedgers(): array;
+
+    /**
+     * @param MutationFilter|null $filter
+     * @return AccountingMutation[]
+     */
     public function getMutations(MutationFilter $filter = null): array;
-    public function addInvoice(array $invoice): string;
+
+    /**
+     * @param AccountingInvoice $invoice
+     * @return string   New invoice number
+     */
+
+    public function addInvoice(AccountingInvoice $invoice): string;
+
+    /**
+     * @param AccountingRelation $relation
+     * @return AccountingRelation
+     */
     public function addRelation(AccountingRelation $relation): AccountingRelation;
+
+    /**
+     * @param AccountingRelation $relation
+     * @return AccountingRelation
+     */
     public function updateRelation(AccountingRelation $relation): AccountingRelation;
 }
